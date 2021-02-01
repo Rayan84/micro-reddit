23a4bb47c0ecc94436171cc9fb3c2b865cd6fc93
Rayan <rayan_84@live.com>
2021-02-01 19:49:40 +0100

    Fix rubocop offenses

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Post < ApplicationRecord
   validates :body, presence: true
   validates :body, length: { minimum: 4 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-class User < ApplicationRecord
+=class User < ApplicationRecord
   has_many :posts
   has_many :comments
   validates :name, presence: true
